e6f31ea4effd3d4121a3180e8300911e32941ab6
xiaoxin <xiaoxin@qq.com>
2019-09-15 20:17:27 +0800

    第一次提交

---
+++ testGit/src/itcast/Test1.java
@@ -0,0 +1,9 @@
+package itcast;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println(888);
+        System.out.println(888);
+        System.out.println(888);
+    }
+}
